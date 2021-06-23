62e62af3a9ea993f22bd1b37daea9e640d70d5e1
Darlan Raimundo <darlanraimundo123@gmail.com>
2021-06-22 23:00:01 -0300

    Auth(first part)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { useState } from 'react';
+import { createContext } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+export const TestContext = createContext({}) as any;
+
 function App() {
+  const [value, setValue] = useState('teste')
+
   return (
-    <h1>Hello world!</h1>
+    <BrowserRouter>
+      <TestContext.Provider value={{ value, setValue }}>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </TestContext.Provider>
+    </BrowserRouter>
   );
 }
 
