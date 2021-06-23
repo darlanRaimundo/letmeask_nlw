0c257014fec6210a55abdd0b9629d408eb33f9a8
DarlanRaimundo <darlanraimundo123@gmail.com>
2021-06-23 13:35:39 -0300

    Authentication (Part 2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,17 @@
-import { useState } from 'react';
-import { createContext } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 
 import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 
-export const TestContext = createContext({}) as any;
+import { AuthContextProvider } from './contexts/AuthContexts';
 
 function App() {
-  const [value, setValue] = useState('teste')
-
   return (
     <BrowserRouter>
-      <TestContext.Provider value={{ value, setValue }}>
+      <AuthContextProvider>
         <Route path="/" exact component={Home} />
         <Route path="/rooms/new" component={NewRoom} />
-      </TestContext.Provider>
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,14 +4,23 @@ import 'firebase/auth';
 import 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID
+    apiKey: "AIzaSyAWBAqwfq43Hij46gPw79ZhfAC7K64UnQY",
+    authDomain: "letmeask-nlw-3bdf8.firebaseapp.com",
+    databaseURL: "https://letmeask-nlw-3bdf8-default-rtdb.firebaseio.com",
+    projectId: "letmeask-nlw-3bdf8",
+    storageBucket: "letmeask-nlw-3bdf8.appspot.com",
+    messagingSenderId: "163514507784",
+    appId: "1:163514507784:web:b58fef628be2327c83e7bd"
 };
+// const firebaseConfig = {
+//     apiKey: process.env.REACT_APP_API_KEY,
+//     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_DATABASE_URL,
+//     projectId: process.env.REACT_APP_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_APP_ID
+// };
 
 firebase.initializeApp(firebaseConfig);
 
